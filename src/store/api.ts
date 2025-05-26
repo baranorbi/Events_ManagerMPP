0eab71b7c9dd449fac907a046ee532c42b5fdf0f
delgiht <baranorbi12@gmail.com>
2025-05-26 11:23:21 +0300

    fixig

--- src/store/api.ts
+++ src/store/api.ts
@@ -5,7 +5,7 @@ const CLOUDFRONT_DOMAIN = 'd1lre8oyraby8d.cloudfront.net';
 const api = axios.create({
   baseURL: import.meta.env.PROD 
     ? `https://${CLOUDFRONT_DOMAIN}/api`
-    : 'http://127.0.0.1:8000/api',
+    : '/api',
   timeout: 60000,
 });
 
